f1e254322cfcced320fa1e7f9ad2b7ff908d744c
Rebecca <reb@bu.edu>
2026-09-15 23:48:08 -0400

    Finalized Code

--- docs/firmware/TimerCode.py
+++ docs/firmware/TimerCode.py
@@ -2,18 +2,19 @@ from machine import Pin, PWM
 import time
 
 #used as guides:
-#motor: https://randomnerdtutorials.com/esp32-stepper-motor-28byj-48-uln2003/
 #pwm: https://linuxhint.com/pwm-esp32-micropython-thonny-ide/
 
 #inputs
 butt1 = Pin(5, Pin.IN, Pin.PULL_UP) #cycle thru 15 20 25 30
 butt2 = Pin(6, Pin.IN, Pin.PULL_UP) #start/reset
 #Pin.PULL_UP is bc there is no resistor attached
+#not press = 1
+#pressed = 0
 
 #leds
 red = PWM(Pin(8)) #timer finished
-blue = PWM(Pin(7)) #waiting
-green = PWM(Pin(9)) #timer working
+green = PWM(Pin(7)) #timer working
+blue = PWM(Pin(9)) #waiting mode
 
 Frequency = 1000 #0 to 78125, website uses 5k
 
@@ -22,9 +23,9 @@ blue.freq(Frequency)
 green.freq(Frequency)
 
 #motor configuration
-STEPS_PER_REVOLUTION = 2048
+STEPS_PER_REVOLUTION = 2048 # like variable says, number of steps to complete 1 revolution
 
-#motor pins
+#motor control pins
 IN1 = Pin(1, Pin.OUT)
 IN2 = Pin(2, Pin.OUT)
 IN3 = Pin(3, Pin.OUT)
@@ -37,52 +38,63 @@ step_sequence = [
     (0, 1, 0, 0),   # Coil 3 - yellow
     (0, 0, 0, 1)    # Coil 4 - blue
 ]
+# since we want to do it in waves, we have to charge it one at a time to use it, moving one coil a little at a time
 
-step_number = 0
+step_number = 0 #keep track of step number, will change thruought
 
 #functions for LED
-def led_off():
+def led_off(): # led off = led off
     red.duty(0)
     blue.duty(0)
     green.duty(0)
 
-def set_blue():
+#blue = waiting to start, can change timer mode
+#green = timer is active
+#red = timer ended, reseting
+def set_blue(): #only blue is on, fully on
     red.duty(0)
     blue.duty(1023)
     green.duty(0)
+#blue is on the whole time bc it serves as a waiting mode
 
-def set_green(brightness):
+def set_green(brightness): # have green be whatever brightness is set to
     red.duty(0)
     blue.duty(0)
     green.duty(brightness)
+#green pulses bc it's "active"
 
-def set_red(brightness):
+def set_red(brightness): # have red be whatever brightness is set to
     red.duty(brightness)
     blue.duty(0)
     green.duty(0)
+#red pulses to alert the user
 
 #reset LED pulsing
 def reset_pulse():
     global pulse_brightness
     global pulse_direction
 
-    pulse_brightness = 0
-    pulse_direction = 1
+    pulse_brightness = 0 #start with led turned off
+    pulse_direction = 1 #1 means the brightness increases
 
 #functions for stepper
-def step_motor(direction):
+def step_motor(direction): #direction 1 -> forward, -1 -> backward
     global step_number
-
-    sequence = step_sequence[step_number]
-
+    
+    sequence = step_sequence[step_number] #get pattern from stepping sequence
+    
+    #pattern order, send it to four motor control pins
     IN1.value(sequence[0])
     IN2.value(sequence[1])
     IN3.value(sequence[2])
     IN4.value(sequence[3])
 
+    #move to next step
     step_number += direction
     step_number = step_number % 4
-
+    #%4 is a remainder of 4 and creates a repeating pattern of 0 1 2 3 0 1 2 3
+    
+#turns off all coils
 def motor_off():
     IN1.value(0)
     IN2.value(0)
@@ -91,51 +103,59 @@ def motor_off():
 
 
 #timer stuff
-presets = [
-    15,
-    20,
-    25,
-    30
-#     15 * 60,    # 15 minutes
-#     20 * 60,    # 20 minutes
-#     25 * 60,    # 25 minutes
-#     30 * 60     # 30 minutes
+presets = [ #options for timers
+    #these were tester values, 15-30 seconds
+#     15,
+#     20,
+#     25,
+#     30
+    15 * 60,    # 15 minutes
+    20 * 60,    # 20 minutes
+    25 * 60,    # 25 minutes
+    30 * 60     # 30 minutes
 ]
 
+full_rev = 30 * 60 #time to get a full turn, * 60 for mins
+
+#keeping track of which timer is active, 0 - 15, 1 - 20, 2 - 25, 3 - 30
 preset_number = 0
 selected_time = presets[preset_number]
-remaining_time = selected_time
+remaining_time = selected_time #time left on timer
+
+#running = running, timer is counting
+# finished = timer ended
+#returning = timer is moving back to orig position
 running = False
 finished = False
 returning = False
 
 #functions for clockhand
 steps_taken = 0
-last_step_time = time.ticks_ms()
-start_time = time.ticks_ms()
-last_return_step_time = time.ticks_ms()
+last_step_time = time.ticks_ms() # stores last time motor moved
+start_time = time.ticks_ms() # stores when timer started
+last_return_step_time = time.ticks_ms() # lat time motor was moved while returning
 
 def get_step_interval():
     # Time for one complete revolution divided by number of steps
-    return (30 * 1000) / STEPS_PER_REVOLUTION
+    return (full_rev * 1000) / STEPS_PER_REVOLUTION
 
-def get_target_steps():
-    return int((selected_time / 30) * STEPS_PER_REVOLUTION)
+def get_target_steps(): #calcs motor steps to time
+    return int((selected_time / full_rev) * STEPS_PER_REVOLUTION)
 
 
 #button setup
 last_butt1 = 1
 last_butt2 = 1
-last_butt_time = time.ticks_ms()
-BUTT_DELAY = 200
+last_butt_time = time.ticks_ms() #debounces basically
+BUTT_DELAY = 200 # min time b/w presses
 
 #led pulsing
 pulse_brightness = 0
 pulse_direction = 1
 
-last_pulse_time = time.ticks_ms()
+last_pulse_time = time.ticks_ms() #stores last time led brightness changed
 
-def update_pulse(color):
+def update_pulse(color): #led slowly brightens and dims
 
     global pulse_brightness
     global pulse_direction
@@ -147,20 +167,22 @@ def update_pulse(color):
     if time.ticks_diff(current_time, last_pulse_time) >= 5:
 
         last_pulse_time = current_time
-
+        
+        #incr or decr brightness
         pulse_brightness += pulse_direction * 10
-
+        
+        #if max brightness, go dimmer
         if pulse_brightness >= 1023:
             pulse_brightness = 1023
             pulse_direction = -1
-
+        #if min brightness, go brighter
         if pulse_brightness <= 0:
             pulse_brightness = 0
             pulse_direction = 1
-
+        #sets green @ brightness level
         if color == "green":
             set_green(pulse_brightness)
-
+        #sets red
         elif color == "red":
             set_red(pulse_brightness)
 
@@ -177,30 +199,39 @@ def reset_timer():
     global last_step_time
     global start_time
     global last_return_step_time
-
+    
+    #put remaining time back to selected time option
     remaining_time = selected_time
+    
+    #stop timer since we are resetting it
     running = False
     finished = False
 
+    #if motor moved, move it back to starting position
     if steps_taken > 0:
         returning = True
-        last_return_step_time = time.ticks_ms()
-    else:
+        last_return_step_time = time.ticks_ms() # when return movement started
+
+        #resetting LED
+        set_red(1023)
+
+    else: #at start, dont move it
         returning = False
         step_number = 0
         motor_off()
 
+        #waiting LED
+        set_blue()
+
     #reset LED pulse
     reset_pulse()
 
-    #waiting LED
-    set_blue()
     last_step_time = time.ticks_ms()
     start_time = time.ticks_ms()
 
 #init
 
-set_blue()
+set_blue() #blue is on when turned on
 reset_pulse()
 motor_off()
 
@@ -212,24 +243,24 @@ while True:
 
     #button 1: cycle thru options
     butt1_state = butt1.value()
-    if butt1_state == 0 and last_butt1 == 1:
+    if butt1_state == 0 and last_butt1 == 1: #debouncing more
 
         #only can change if preset mode is active
         if not running and not finished and not returning:
-            preset_number += 1
-            if preset_number >= len(presets):
+            preset_number += 1 # move to next preset option
+            if preset_number >= len(presets): # cycle back to first preset when preset list is exhausted
                 preset_number = 0
-            selected_time = presets[preset_number]
-            remaining_time = selected_time
+            selected_time = presets[preset_number] #get new selected time
+            remaining_time = selected_time #reset remaining time to new option in case it held old one
             finished = False
-            print(
+            print( #print to thonny just in case
                 "Timer:",
-                selected_time,# // 60,
+                selected_time //60, #divide to get minutes
                 "minutes"
             )
             set_blue()
 
-    last_butt1 = butt1_state
+    last_butt1 = butt1_state # remember current state
 
     #button 2: start/reset
     butt2_state = butt2.value()
@@ -254,7 +285,7 @@ while True:
                 reset_pulse()
                 last_step_time = current_time
                 start_time = current_time
-                print("Timer started")
+                print("Timer started") #for testing tbh
 
     last_butt2 = butt2_state
 
@@ -265,21 +296,20 @@ while True:
         update_pulse("green")
 
         #move hand
-        target_steps = get_target_steps()
-        step_interval = get_step_interval()
+        elapsed_seconds = time.ticks_diff(current_time, start_time) / 1000
 
-        if steps_taken < target_steps:
-            if time.ticks_diff(current_time, last_step_time) >= step_interval:
-                last_step_time = current_time
-                step_motor(1)
-                steps_taken += 1
+        #calculate how many steps the hand should have taken by now
+        target_steps = int((elapsed_seconds / full_rev) * STEPS_PER_REVOLUTION)
+
+        if steps_taken < target_steps: #if not at correct step, move it forward
+            step_motor(1)
+            steps_taken += 1
 
         #check remaining time
-        elapsed_seconds = time.ticks_diff(current_time, start_time) / 1000
         remaining_time = selected_time - elapsed_seconds
 
         #timer finished
-        if elapsed_seconds >= selected_time:
+        if elapsed_seconds >= selected_time: #reseting !
             remaining_time = 0
             running = False
             finished = True
@@ -300,27 +330,36 @@ while True:
         #return hand to starting position
         if returning:
 
-            if steps_taken > 0:
-
+            if steps_taken > 0: #if not back at starting position
+                
+                #moving it back !!!
                 if time.ticks_diff(current_time, last_return_step_time) >= 1: #smaller number = faster
                     last_return_step_time = current_time
-                    step_motor(-1)
-                    steps_taken -= 1
+                    step_motor(-1) # move 1 step back
+                    steps_taken -= 1 #count that ^
 
-            else:
+            else: #made it to start
                 returning = False
                 step_number = 0
                 motor_off()
 
+                #waiting LED
+                set_blue()
+
     #waiting for timer
     else:
-        set_blue()
+
+        #blue LED only when NOT resetting
+        if not returning:
+            set_blue()
 
         #return hand to starting position
         if returning:
 
-            if steps_taken > 0:
-
+            if steps_taken > 0: #checking for steps leftover
+                
+                
+                #move motor backward quickly
                 if time.ticks_diff(current_time, last_return_step_time) >= 1:
                     last_return_step_time = current_time
                     step_motor(-1)
@@ -330,6 +369,9 @@ while True:
                 returning = False
                 step_number = 0
                 motor_off()
+
+                #waiting LED
+                set_blue()
 
     #lil delay
     time.sleep_ms(5)
